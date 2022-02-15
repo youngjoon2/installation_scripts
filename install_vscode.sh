18664e554de901a24118a1af4e9b9956b5b3aece
Youngjoon Choi <youngj1121@naver.com>
2022-02-15 18:51:50 +0900

    first commit

---
+++ install_vscode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install -y curl
+sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
+
